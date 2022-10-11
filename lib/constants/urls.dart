509d36970f297b7953563eb034d5a4e363d019e3
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-10-12 01:10:41 +0200

    Changed more info custom filters url

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,5 +1,5 @@
 class Urls {
   static const String playStore = "https://play.google.com/store/apps/details?id=com.jgeek00.adguard_home_manager";
   static const String gitHub = "https://github.com/JGeek00/adguard-home-manager";
-  static const String customRuleDocs = "https://github.com/AdguardTeam/AdGuardHome/wiki/Hosts-Blocklists";
+  static const String customRuleDocs = "https://kb.adguard.com/en/general/how-to-create-your-own-ad-filters";
 }
