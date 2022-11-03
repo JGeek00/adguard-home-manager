286e46d4d8157ebd8878db5a5f09f7b8bca6358a
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-11-03 14:51:32 +0100

    Changed logs screen

--- lib/screens/logs/log_tile.dart
+++ lib/screens/logs/log_tile.dart
@@ -2,17 +2,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:bottom_sheet/bottom_sheet.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'package:adguard_home_manager/screens/logs/log_details_modal.dart';
+import 'package:adguard_home_manager/screens/logs/log_details_screen.dart';
 
 import 'package:adguard_home_manager/providers/app_config_provider.dart';
 import 'package:adguard_home_manager/functions/get_filtered_status.dart';
-import 'package:adguard_home_manager/models/filtering_status.dart';
-import 'package:adguard_home_manager/classes/process_modal.dart';
-import 'package:adguard_home_manager/providers/servers_provider.dart';
-import 'package:adguard_home_manager/services/http_requests.dart';
 import 'package:adguard_home_manager/models/logs.dart';
 import 'package:adguard_home_manager/functions/format_time.dart';
 
@@ -30,7 +24,6 @@ class LogTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final serversProvider = Provider.of<ServersProvider>(context);
     final appConfigProvider = Provider.of<AppConfigProvider>(context);
 
     final width = MediaQuery.of(context).size.width;
@@ -70,87 +63,16 @@ class LogTile extends StatelessWidget {
         text: filter['label'],
       );
     }
-
-    void blockUnblock(Log log, String newStatus) async {
-      final ProcessModal processModal = ProcessModal(context: context);
-      processModal.open(AppLocalizations.of(context)!.savingUserFilters);
-
-      final rules = await getFilteringRules(server: serversProvider.selectedServer!);
-
-      if (rules['result'] == 'success') {
-        FilteringStatus oldStatus = serversProvider.filteringStatus!;
-
-        List<String> newRules = rules['data'].userRules.where((domain) => !domain.contains(log.question.name)).toList();
-        if (newStatus == 'block') {
-          newRules.add("||${log.question.name}^");
-        }
-        else if (newStatus == 'unblock') {
-          newRules.add("@@||${log.question.name}^");
-        }
-        FilteringStatus newObj = serversProvider.filteringStatus!;
-        newObj.userRules = newRules;
-        serversProvider.setFilteringStatus(newObj);
-
-        final result  = await postFilteringRules(server: serversProvider.selectedServer!, data: {'rules': newRules});
-        
-        processModal.close();
-        
-        if (result['result'] == 'success') {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(AppLocalizations.of(context)!.userFilteringRulesUpdated),
-              backgroundColor: Colors.green,
-            )
-          );
-        }
-        else {
-          appConfigProvider.addLog(result['log']);
-          serversProvider.setFilteringStatus(oldStatus);
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(AppLocalizations.of(context)!.userFilteringRulesNotUpdated),
-              backgroundColor: Colors.red,
-            )
-          );
-        }
-      }
-      else {
-        appConfigProvider.addLog(rules['log']);
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(AppLocalizations.of(context)!.userFilteringRulesNotUpdated),
-            backgroundColor: Colors.red,
-          )
-        );
-      }
-    }
-    
-    void openLogDetailsModal() {
-      ScaffoldMessenger.of(context).clearSnackBars();
-      showFlexibleBottomSheet(
-        minHeight: 0.6,
-        initHeight: 0.6,
-        maxHeight: 0.95,
-        isCollapsible: true,
-        duration: const Duration(milliseconds: 250),
-        anchors: [0.95],
-        context: context, 
-        builder: (ctx, controller, offset) => LogDetailsModal(
-          scrollController: controller,
-          log: log,
-          blockUnblock: blockUnblock,
-        ),
-        bottomSheetColor: Colors.transparent,
-      );
-    }
-
+  
     return Material(
       color: Colors.transparent,
       child: InkWell(
-        onTap: openLogDetailsModal,
+        onTap: () => Navigator.push(context, MaterialPageRoute(
+          builder: (context) => LogDetailsScreen(log: log)
+        )),
         child: Container(
           width: double.maxFinite,
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -161,12 +83,13 @@ class LogTile extends StatelessWidget {
                   children: [
                     Text(
                       log.question.name,
-                      overflow: TextOverflow.ellipsis,
                       style: const TextStyle(
-                        fontSize: 18
+                        fontSize: 16,
+                        height: 1.5,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
-                    const SizedBox(height: 10),
+                    const SizedBox(height: 5),
                     if (log.client.length <= 15 && appConfigProvider.showNameTimeLogs == false) Row(
                       children: [
                         ...[
@@ -182,7 +105,9 @@ class LogTile extends StatelessWidget {
                               overflow: TextOverflow.ellipsis,
                               style: TextStyle(
                                 color: Theme.of(context).listTileTheme.iconColor,
-                                fontSize: 13
+                                fontSize: 14,
+                                height: 1.4,
+                                fontWeight: FontWeight.w400,
                               ),
                             ),
                           )
