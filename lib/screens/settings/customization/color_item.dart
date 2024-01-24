65ee702b8962f00a401783cc5ea6864d0105ff37
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-01-24 13:07:25 +0100

    Improved theme color selection for desktop

--- lib/screens/settings/customization/color_item.dart
+++ lib/screens/settings/customization/color_item.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
+  final int index;
+  final int total;
   final Color color;
   final int numericValue;
   final int? selectedValue;
   final void Function(int) onChanged;
 
   const ColorItem({
-    Key? key,
+    super.key,
+    required this.index,
+    required this.total,
     required this.color,
     required this.numericValue,
     required this.selectedValue,
     required this.onChanged
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: index == 0
+        ? const EdgeInsets.only(top: 10, right: 10, bottom: 10)
+        : index == total-1
+          ? const EdgeInsets.only(top: 10, bottom: 10, left: 10)
+          : const EdgeInsets.all(10),
       child: Material(
         borderRadius: BorderRadius.circular(50),
         child: InkWell(
