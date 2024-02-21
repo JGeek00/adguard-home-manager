e603814d42834ebeda1d7e3015674536ac3c99eb
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-02-21 10:50:33 +0100

    Removed unused imports

--- lib/screens/settings/server_info/server_info.dart
+++ lib/screens/settings/server_info/server_info.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:animations/animations.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
