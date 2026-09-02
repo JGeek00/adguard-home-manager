c0cf4d35815246c5ae059919712687a76872c2e0
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-09-02 16:00:33 +0200

    Updates

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   dynamic_color
   screen_retriever_linux
   sentry_flutter
+  sqlite3_flutter_libs
   url_launcher_linux
   window_manager
 )
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,6 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   dynamic_color
   screen_retriever_windows
   sentry_flutter
+  sqlite3_flutter_libs
   url_launcher_windows
   window_manager
 )
